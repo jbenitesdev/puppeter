eea8045a59196432725290ca1d07b37ce5a5871a
Joao Benites <joao.alvesbenites@gmail.com>
2020-01-31 13:50:28 -0300

    Cors

--- src/service/server.js
+++ src/service/server.js
@@ -6,13 +6,7 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Codingpedia, Authorization');
-    next();
-})
+app.use(cors())
 
 app.use('/',require('./routes'))
 
